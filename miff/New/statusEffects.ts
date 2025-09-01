413bfec137076f9176a44f5c9753816c6b1b7881
rcbiscuitsbelfast-prog <r.c.biscuits.belfast@gmail.com>
2025-09-01 17:49:07 +0100

    Create statusEffects.ts

---
+++ miff/New/statusEffects.ts
@@ -0,0 +1,27 @@
+export interface StatusEffect {
+  id: string;
+  name: string;
+  duration: number;
+  impact: string;
+}
+
+const activeEffects: StatusEffect[] = [];
+
+export function applyStatusEffect(effect: StatusEffect): void {
+  activeEffects.push(effect);
+}
+
+export function tickStatusEffects(): void {
+  for (const effect of activeEffects) {
+    effect.duration -= 1;
+  }
+  for (let i = activeEffects.length - 1; i >= 0; i--) {
+    if (activeEffects[i].duration <= 0) {
+      activeEffects.splice(i, 1);
+    }
+  }
+}
+
+export function listStatusEffects(): StatusEffect[] {
+  return [...activeEffects];
+}
