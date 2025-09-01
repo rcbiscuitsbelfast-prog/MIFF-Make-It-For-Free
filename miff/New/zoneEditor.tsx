4cb1c22c598bbadd5e48f4aeabc0c78b907ec678
rcbiscuitsbelfast-prog <r.c.biscuits.belfast@gmail.com>
2025-09-01 17:49:48 +0100

    Create zoneEditor.tsx

---
+++ miff/New/zoneEditor.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { TileManager } from '../TileMapPure/tileManager';
+import { TileType } from '../TileMapPure/tileTypes';
+
+interface ZoneEditorProps {
+  manager: TileManager;
+  onSave: () => void;
+}
+
+export const ZoneEditor: React.FC<ZoneEditorProps> = ({ manager, onSave }) => {
+  const handleClick = (x: number, y: number) => {
+    manager.setTile(x, y, TileType.Forest);
+  };
+
+  return (
+    <div>
+      <h3>Zone Editor</h3>
+      <button onClick={onSave}>Save Zone</button>
+      <div style={{ display: 'grid', gridTemplateColumns: `repeat(${manager.getGrid()[0].length}, 32px)` }}>
+        {manager.getGrid().flat().map(tile => (
+          <div
+            key={`${tile.x}-${tile.y}`}
+            onClick={() => handleClick(tile.x, tile.y)}
+            style={{
+              width: '32px',
+              height: '32px',
+              backgroundColor: '#ccc',
+              border: '1px solid #333',
+              fontSize: '0.6rem',
+              textAlign: 'center',
+            }}
+          >
+            {tile.type[0]}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
